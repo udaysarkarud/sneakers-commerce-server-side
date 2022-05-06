e9795635f32f5bc4c3a50bdd6a5710202c8f6778
udaysarkarud <udaysarkarud@gmail.com>
2022-05-06 18:10:58 +0600

    added new smtp

--- index.js
+++ index.js
@@ -220,31 +220,31 @@ const main = async () => {
         // nodemailer
         app.post('/send-confirmation-email', async (req, res) => {
             const mailInfo = req.body;
-            console.log(mailInfo)
-
             const transporter = nodemailer.createTransport({
-                server: 'premium212.web-hosting.com',
+                host: "premium212.web-hosting.com",
                 port: 587,
-                secure: true, // use SSL
+                secure: false,
                 auth: {
-                    user: `${process.env.NODEMAIL_MAILADD}`,
-                    pass: `${process.env.NODEMAIL_MAILPASS}`
+                    user: 'infomng@sedsbd.org',
+                    pass: 'FtiSoDx7RBv#'
                 }
             });
 
             // send mail with defined transport object
             let info = await transporter.sendMail({
-                from: `${process.env.NODEMAIL_MAILADD}`, // sender address
+                from: 'infomng@sedsbd.org', // sender address
                 to: `${mailInfo.clientEmail}`, // list of receivers
                 subject: "Order confirmation from sneaker.com", // Subject line
                 text: `Dear ${mailInfo.clientName}, We are happy to let you know that we have received your order.Your order details can be found below. Once your package ships, we will send you an email with a confirm so you can see the movement of your package. Order Number: ${mailInfo.orderId}, Order Date</b>: ${mailInfo.orderDate}</p><p><b>Order Total</b>: ${mailInfo.totalAmount}</p><p><b>Payment ID</b>: ${mailInfo.paymentid}</p><p><b>SHIPPING ADDRESS</b>: ${mailInfo.clientAddress}</p><br/><p>You can review your order status at any time by visiting Your Account. We hope you enjoyed your shopping experience with us and that you will visit us again soon.If you have any questions, contact us here or call us on +8801711227383.We are here to help!</p>`, // plain text body
                 html: `<p>Dear <b>${mailInfo.clientName},</b></p><p>We are happy to let you know that we have received your order.Your order details can be found below. Once your package ships, we will send you an email with a confirm so you can see the movement of your package.</p><br/><p><u>ORDER SUMMARY</u></p><p><b>Order Number:</b> ${mailInfo.orderId}</p><p><b>Order Date</b>: ${mailInfo.orderDate}</p><p><b>Order Total</b>: ${mailInfo.totalAmount}</p><p><b>Payment ID</b>: ${mailInfo.paymentid}</p><p><b>SHIPPING ADDRESS</b>: ${mailInfo.clientAddress}</p><br/><p>You can review your order status at any time by visiting Your Account. We hope you enjoyed your shopping experience with us and that you will visit us again soon.If you have any questions, contact us here or call us on +8801711227383.We are here to help!</p>` // html body
             }, (error, info) => {
                 if (error) {
-                    console.log('error on sending mail', error)
+                    //console.log('error on sending mail', error)
+                    res.status(500).send("Internal Server Error");
                 }
                 else {
-                    console.log('email send perfectly', info)
+                    //console.log('email send perfectly', info)
+                    res.status(200).send("OK");
                 }
             });
 
